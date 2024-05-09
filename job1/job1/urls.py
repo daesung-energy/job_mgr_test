113f898fbdf09b99ae24d480520f1d25b4322c72
daesung-energy <DSEIT21@daesung.com>
2024-05-09 10:46:02 +0900

    최신 개발

--- job1/job1/urls.py
+++ job1/job1/urls.py
@@ -18,10 +18,22 @@ from django.contrib import admin
 from django.urls import include, path
 from jobs import views
 
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import re_path as url
+from django.views.static import serve
+
 urlpatterns = [
     path('jobs/', include('jobs.urls')), #jobs로 시작하는 모든 url은 jobs>urls.py 파일을 참조할 것이다. 즉 jobs부터 치고 다음 주소를 쳐야 한다.
     #path("users/", include('users.urls')), #users으로 시작하는 모든 url은 모두 users>urls.py 파일을 참조할 것이다.
     path("admin/", admin.site.urls), #admin치고 들어가면 됨
     path('job2/', include('job2.urls')), #job2로 시작하는 모든 url은 job2>urls.py 파일을 참조할 것이다. 즉 job2부터 치고 다음 주소를 쳐야 한다.
     path('common/', include('common.urls')), #common으로 시작하는 모든 url은 common/urls.py 파일을 참조할 것이다.
+]
+
+# 배포시 추가
+urlpatterns += [
+        url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
