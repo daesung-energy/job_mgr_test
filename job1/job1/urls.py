ea055a0b1ad682a833bf7c05c8f4693df1c1e1d8
daesung-energy <DSEIT21@daesung.com>
2024-05-09 14:25:33 +0900

    주소생

--- job1/job1/urls.py
+++ job1/job1/urls.py
@@ -29,7 +29,8 @@ urlpatterns = [
     #path("users/", include('users.urls')), #users으로 시작하는 모든 url은 모두 users>urls.py 파일을 참조할 것이다.
     path("admin/", admin.site.urls), #admin치고 들어가면 됨
     path('job2/', include('job2.urls')), #job2로 시작하는 모든 url은 job2>urls.py 파일을 참조할 것이다. 즉 job2부터 치고 다음 주소를 쳐야 한다.
-    path('common/', include('common.urls')), #common으로 시작하는 모든 url은 common/urls.py 파일을 참조할 것이다.
+    # path('common/', include('common.urls')), #common으로 시작하는 모든 url은 common/urls.py 파일을 참조할 것이다.
+    path('', include('common.urls'))
 ]
 
 # 배포시 추가
